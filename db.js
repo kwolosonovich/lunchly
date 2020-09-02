ef78b3b7bcacf0b8351e8ffcafd195ae2213ebbb
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-09-02 12:40:17 -0700

    updated db

--- db.js
+++ db.js
@@ -1,8 +1,27 @@
 /** Database for lunchly */
 
-const pg = require("pg");
+// const pg = require("pg");
 
-const db = new pg.Client("postgresql:///lunchly");
+// const db = new pg.Client("postgresql:///lunchly");
+
+// db.connect();
+
+// module.exports = db;
+
+
+const { Client } = require("pg");
+
+let data;
+
+if (process.env.NODE_ENV === "test") {
+  data = "lunchly_test";
+} else {
+  data = "lunchly";
+}
+
+let db = new Client({
+  database: data,
+});
 
 db.connect();
 
